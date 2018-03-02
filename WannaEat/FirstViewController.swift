8eb9b5d61281d19f2c984bb4b635088ae4b05254
x1Dman <glaider102@mail.ru>
2018-03-02 14:22:08 +0300

    Initial Commit

---
+++ WannaEat/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WannaEat
+//
+//  Created by Apple on 02/03/2018.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
